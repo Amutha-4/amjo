85e755643309e28b1cc4ab4277efb3464f16a667
Amutha-4 <amutharam04041999@gmail.com>
2018-09-24 17:53:43 +0530

    Create alphabetornot.py

---
+++ alphabetornot.py
@@ -0,0 +1,8 @@
+print("enter'0' for exit.");
+ch=input("enter any character:");
+if ch=='0':
+  exit();
+else:
+   if((ch>='a' and ch<='Z')or(ch,"is an alphabet.");
+   else:
+   print(ch," not an alphabet.")
