051357384e20b694852fd8e23b91fdb981d660e6
Amutha-4 <amutharam04041999@gmail.com>
2018-09-27 17:07:26 +0530

    Create sumofnaturalno.py

---
+++ sumofnaturalno.py
@@ -0,0 +1,7 @@
+number=int(input("please enter any number:"))
+total=0
+value=1
+while(value<=number):
+  total=total+value
+  value=value+1
+print("the sum of natural number from 1 to{0}".fomat(number,total))
