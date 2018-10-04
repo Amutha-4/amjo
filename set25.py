ef789e264a7019bf9e49b1448b919a144a5c2d55
Amutha-4 <amutharam04041999@gmail.com>
2018-10-04 17:17:50 +0530

    Create set25.py

---
+++ set25.py
@@ -0,0 +1,11 @@
+using namespace std:
+void klargest(int arr[]' int n )
+sort(arr, arr+n,greater<int>()):
+for(int i=0:i<k;i++)
+count<<arr[i]<<"";
+}
+int main()
+{
+int arr[]={1,2,3,4,}
+int n=size of [arr]
+}
