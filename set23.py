bb3e989488ba37d30a90ffa8b47adf77e20dc4a2
Amutha-4 <amutharam04041999@gmail.com>
2018-10-04 16:57:00 +0530

    Create set23.py

---
+++ set23.py
@@ -0,0 +1,13 @@
+using namespace  std:
+void klargest(int arr[],int n,int k)
+sort(arr,arr+n,greater<int>()):
+for(int i=0:i<k:i++)
+count<<arr[i]<<"":
+}
+int main ()
+{
+int arr[]={1,23,12,9,30,2,50}
+int n=size of [arr]
+int k=3:
+klangerst(arr,n,k):
+}
