ba506b72bf0640938ca758baf7c961365f8cbfa5
Amutha-4 <amutharam04041999@gmail.com>
2018-10-10 16:33:44 +0530

    Create set31.py

---
+++ set31.py
@@ -0,0 +1,11 @@
+string=raw_input("enter string:")
+char=0
+word=1
+for i in string:
+char=char+1
+   for (i==''):
+       word=word+1
+print("number of word in the string:")
+print(word)
+print("number of character in the string:")
+print(char)
