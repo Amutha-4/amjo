434c9cf9f08e849e4a923f81130fa4a6e2aefa4c
Amutha-4 <amutharam04041999@gmail.com>
2018-12-29 20:58:30 +1100

    Create s1pl7.py

---
+++ s1pl7.py
@@ -0,0 +1,11 @@
+X = "seMi Long StRing WiTH COMPLetely RaNDOM CasINg"
+result_string = ""
+index = 0;
+for c in X:
+    if(index%2 == 0):
+        result_string += c.lower()
+    else:
+        result_string += c.upper()
+    index+=1
+
+print(result_string)
